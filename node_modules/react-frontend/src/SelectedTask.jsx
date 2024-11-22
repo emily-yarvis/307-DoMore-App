f840a714a4ebf587cf4e7d9a064b3eadf16633e8
emily-yarvis <eyarvis@calpoly.edu>
2024-11-21 21:25:11 -0800

    Trying to do half of CD without backend frontend connection and doing half of the auth stuff

---
+++ node_modules/react-frontend/src/SelectedTask.jsx
@@ -0,0 +1,24 @@
+// import React from "react";
+import PropTypes from "prop-types";
+
+const SelectedTask = ({ taskName, dueDate, description }) => {
+  return (
+    <div className="p-4 bg-white shadow-lg rounded-md max-w-sm">
+      <h2 className="text-2xl font-semibold mb-2">{taskName}</h2>
+      <p className="text-gray-700 mb-2">
+        <strong>Due Date:</strong> {dueDate}
+      </p>
+      <p className="text-gray-700">
+        <strong>Description:</strong> {description}
+      </p>
+    </div>
+  );
+};
+
+SelectedTask.propTypes = {
+  taskName: PropTypes.string.isRequired,
+  dueDate: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+};
+
+export default SelectedTask;
