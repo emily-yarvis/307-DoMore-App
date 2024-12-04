6af95baf1ac868b629bbacde8eb697ce1d8fe183
emily-yarvis <eyarvis@calpoly.edu>
2024-12-04 12:52:08 -0800

    Fixing connection

--- packages/express-backend/src/backend.js
+++ packages/express-backend/src/backend.js
@@ -12,7 +12,7 @@ const app = express();
 const port = 8000;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: 'https://thankful-stone-03264d61e.5.azurestaticapps.net' }));
 
 mongoose.set("debug", true);
 mongoose
